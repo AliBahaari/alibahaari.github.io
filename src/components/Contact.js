df2d9c1632b57a724cdaf49bf0bd136bda22893f
Ali Bahaari <alibahaari@gmail.com>
2020-11-29 20:03:43 +0330

    Updated Social Networks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,7 @@ import '../styles/Contact.css';
 // Images
 import Dribbble from '../images/dribbble.svg';
 import GitHub from '../images/github.svg';
-import Instagram from '../images/instagram.svg';
 import Telegram from '../images/telegram.svg';
-import Twitter from '../images/twitter.svg';
 
 const Contact = () => {
   return (
@@ -21,12 +19,6 @@ const Contact = () => {
         </a>
         <a href="https://dribbble.com/AliBahaari" id="dribbble">
           <img src={ Dribbble } alt="Dribbble" />
-        </a>
-        <a href="https://instagram.com/AliBahaari" id="instagram">
-          <img src={ Instagram } alt="Instagram" />
-        </a>
-        <a href="https://www.twitter.com/AliBahaari" id="twitter">
-          <img src={ Twitter } alt="Twitter" />
         </a>
         <a href="https://t.me/AliBahaari" id="telegram">
           <img src={ Telegram } alt="Telegram" />
