4d789cef3605abf2a3a3dabf57c54d0c941d6da2
Ali Bahaari <alibahaari1997@gmail.com>
2021-08-17 17:55:55 +0430

    Corrected Commas

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -15,9 +15,9 @@ const Introduction = () => {
       <p>
         روان شناسی خوانده ام با اینکه برای مسائل بنیادین انسان هیچ وقت جوابی پیدا نکردم،<br />
         بازار های مالی را با استفاده از استراتژی MRM، استراتژی Ichimoku و امواج Elliott به سبک Robert Prechter تحلیل می کنم،<br />
-        به Python و فریمورک Django تسلط دارم,<br />
-        به مباحث Front-End و به خصوص زبان JavaScript و پلتفرم Node.js تسلط دارم,<br />
-        عاشق React و React Native هستم,<br />
+        به Python و فریمورک Django تسلط دارم،<br />
+        به مباحث Front-End و به خصوص زبان JavaScript و پلتفرم Node.js تسلط دارم،<br />
+        عاشق React و React Native هستم،<br />
         به نرم افزار PsychoPy و برنامه نویسی آن تسلط دارم و تسک های نوروساینس می سازم و<br />
         با Adobe Ilustrator طراحی می کنم.
       </p>
