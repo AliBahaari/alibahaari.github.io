3d834fe70d52071e3b1c048b3b7f93cc7a82a39c
Ali <alibahaari1997@gmail.com>
2024-01-15 19:49:20 +0330

    Modified Introduction Text

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -21,10 +21,10 @@ const Introduction = () => {
         using Python ends up implementing Neuroscience tasks with PsychoPy
         software. Also I use Python for Web Crawling, Ethical Hacking, Financial
         Tools, etc. In addition, I utilize Adobe Illustrator for Graphic
-        Designing such as Logos, Vectors, Icons. Surely, Adobe XD and Figma have
-        been using for UIs. At the end, I analyze Financial Markets. I use
-        different approaches to do so simultaneously. Favorites are Elliott, MRM
-        and Ichimoku.
+        Designing such as Logos, Vectors, Icons, etc.. Surely, Adobe XD and
+        Figma have been using for UIs. At the end, I analyze Financial Markets.
+        I use different approaches to do so simultaneously. Favorites are
+        Elliott, MRM and Ichimoku.
       </p>
     </div>
   )
