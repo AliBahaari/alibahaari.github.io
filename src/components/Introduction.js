1335f2efbd9211d0a08366d5a05acc8b1ec96b11
Ali <alibahaari1997@gmail.com>
2024-02-25 21:53:34 +0330

    Modified Introduction Text

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -13,18 +13,23 @@ const Introduction = () => {
 
       <h1>This Is Me! Ali Bahaari...</h1>
       <p>
-        I'm a Full Stack developer who has expertise in JavaScript, TypeScript,
-        Node.js, React.js, React-Native, Next.js, Jest, Express.js, NestJS,
-        MySQL, Nginx and Git. Certainly HTML and CSS (Sass and SCSS) are
-        included alongside many frameworks and libraries such as Tailwind CSS,
+        I'm a Full Stack Developer who has expertise in JavaScript, TypeScript,
+        React.js, React Native, Next.js, Redux (Toolkit), Zustand, RTK Query,
+        TanStack Query, AG-Grid, Three.js, GSAP, Jest, React Testing Library,
+        Express.js, NestJS, MySQL, Nginx and Git.
+        <br />
+        Certainly HTML and CSS (SCSS) are included alongside Tailwind CSS,
         Material UI, Bootstrap, etc. Also I have expertise in Python. Sometimes
         using Python ends up implementing Neuroscience tasks with PsychoPy
         software. Also I use Python for Web Crawling, Ethical Hacking, Financial
-        Tools, etc. In addition, I utilize Adobe Illustrator for Graphic
-        Designing such as Logos, Vectors, Icons, etc.. Surely, Adobe XD and
-        Figma have been using for UIs. At the end, I analyze Financial Markets.
-        I use different approaches to do so simultaneously. Favorites are
-        Elliott, MRM and Ichimoku.
+        Tools, etc.
+        <br />
+        In addition, I utilize Adobe Illustrator for Graphic Designing such as
+        Logos, Vectors, Icons, etc. Surely, Adobe XD and Figma have been using
+        for UIs.
+        <br />
+        At the end, I analyze Financial Markets. I use different approaches to
+        do so simultaneously. My favorite one is Elliott.
       </p>
     </div>
   )
