b513688308dcdbcc6b3c56737ed23e570aafaf88
Ali <alibahaari1997@gmail.com>
2024-11-16 16:17:53 +0330

    fix: modified introduction text

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -13,14 +13,14 @@ const Introduction = () => {
 
       <h1>This is me! Ali Bahaari..</h1>
       <p>
-        I am a skilled Full Stack and Mobile Developer with extensive experience
+        I am a skilled full stack and mobile developer with extensive experience
         in various technologies, including frameworks and libraries.
         <br />
         My expertise in Python allows me to handle a wide range of tasks, from
         Neuroscience experiments with PsychoPy software to web crawling, ethical
         hacking, and financial bots development.
         <br />I am also passionate about analyzing financial markets, using
-        multiple approaches with a special interest in the Elliott Wave Theory.
+        multiple approaches with a special interest in the Elliott.
       </p>
     </div>
   )
